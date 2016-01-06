27cee0b890a70d28912e57428546714b05766a37
Gregor Rot <gregor.rot@gmail.com>
2016-01-06 09:54:11 +0100

    Bowtie

---
+++ map/bowtie.py
@@ -0,0 +1,28 @@
+import pybio
+from os.path import join as pjoin
+import os
+import sys
+import locale
+
+def bowtie(genome, fastq_file, output_folder, name, cpu=1, verbose=True):
+    """
+    Use the `bowtie aligner <http://bowtie-bio.sourceforge.net/manual.shtml>`_ to map single-end reads to the reference genome.
+
+    :param genome: the genome ID to which to map the reads to
+    :param fastq_file: path to fastq file (can be gzipped)
+    :param output_folder: results will be stored in this folder
+    :param name: name of results
+    :param cpu: number of cores to use
+    :param verbose: print executed STAR commands
+    """
+
+    f = pybio.data.Fastq(fastq_file)
+    f.read()
+    seq_len = len(f.sequence)
+
+    genome_index = os.path.join(pybio.path.genomes_folder, "%s_indices/%s.bowtie" % (genome, genome))
+    command = "pybio.bowtie %s %s %s %s %s %s" % (output_folder, genome_index, os.path.abspath(fastq_file), name, cpu, seq_len)
+    if verbose:
+        print command
+    output, error = pybio.utils.Cmd(command).run()
+    return output
