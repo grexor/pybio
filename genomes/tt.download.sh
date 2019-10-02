3a6cd6479a65dcd8c38a2663e7431d1e7661664f
Gregor Rot <gregor.rot@gmail.com>
2019-10-02 10:04:55 +0200

    Helper script to process tt genome annotation

--- genomes/tt.download.sh
+++ genomes/tt.download.sh
@@ -7,6 +7,16 @@ cd ..
 
 mkdir tt.annotation.custom
 cd tt.annotation.custom
-wget http://www.ciliate.org/system/downloads/T_thermophila_June2014.gff3 -O tt.gff3
+wget http://www.ciliate.org/system/downloads/T_thermophila_June2014.gff3 -O tt.gff
+
+cd ..
+
+python tt_gff_gtf.py
+
+gzip -f tt.annotation.custom/tt.gff
 
 printf 'import pybio\npybio.genomes.prepare("tt")' | python
+
+rm -r tt.assembly.custom.star
+mkdir tt.assembly.custom.star
+STAR --runMode genomeGenerate --genomeDir tt.assembly.custom.star --genomeFastaFiles tt.assembly.custom/tt.fasta --genomeSAindexNbases 12 --runThreadN 4
