c1f64a2d63f758fd337c6efea048721d709b966c
Gregor Rot <gregor.rot@gmail.com>
2021-02-08 18:56:46 +0100

    Config update

--- config/__init__.py
+++ config/__init__.py
@@ -5,6 +5,7 @@ version = 1.3
 
 def init():
     config_file = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", "config", "config.txt"))
-    config_lines = open(config_file).readlines()
-    for cline in config_lines:
-        exec(cline.replace("\r", "").replace("\n", ""))
+    if os.path.exists(config_file):
+        config_lines = open(config_file).readlines()        
+        for cline in config_lines:
+            exec(cline.replace("\r", "").replace("\n", ""))
