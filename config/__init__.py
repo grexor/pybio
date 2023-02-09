4c6781a116d99eae6bc03f86da4721092b928e29
Gregor Rot <gregor.rot@gmail.com>
2023-02-09 10:46:09 +0100

    Genomes config changes

--- config/__init__.py
+++ config/__init__.py
@@ -1,11 +1,17 @@
 import pybio
 import os
+import sys
 
 version = 1.3
 
 def init():
-    config_file = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", "config", "config.txt"))
+    config_module = sys.modules[__name__]
+    config_file = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", "pybio.config"))
     if os.path.exists(config_file):
-        config_lines = open(config_file).readlines()        
+        config_lines = open(config_file).readlines()
         for cline in config_lines:
-            exec(cline.replace("\r", "").replace("\n", ""))
+            k, v = cline.split("=")
+            setattr(config_module, k, eval(v))
+            #exec(cline.replace("\r", "").replace("\n", ""))
+    else:
+        print(f"[config] expected config file at {config_file} not found, using defaults")
