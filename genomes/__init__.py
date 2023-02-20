b5c561ac78aee7e9ee053d483eefe7759db53fd3
Gregor Rot <gregor.rot@gmail.com>
2023-02-20 13:48:26 +0100

    Start of transfer of new annotation to the genomes module

--- genomes/__init__.py
+++ genomes/__init__.py
@@ -8,6 +8,16 @@ import struct
 import itertools
 import copy
 import gzip
+import psutil
+import pickle
+
+process = psutil.Process(os.getpid())
+
+genes_db = {}
+transcripts_db = {}
+exons_db = {}
+gene_bins_db = {}
+gene_bin_size = 100000
 
 code = {"R": ["A", "G"], "Y": ["C", "T"], "S": ["G", "C"], "W": ["A", "T"]}
 revCode = {'A': 'T', 'T': 'A', 'U': 'A', 'G': 'C', 'C': 'G', 'R': 'Y', 'Y': 'R', 'K': 'M', 'M': 'K', 'S': 'S', 'W': 'W', 'B': 'V', 'D': 'H', 'H': 'D', 'V': 'B', 'N': 'N'}
@@ -19,6 +29,55 @@ chr_ensembl_ucsc = {}
 genes = {}
 intervals = {}
 linear = {}
+
+class Gene:
+
+  def __init__(self, gene_id, gene_name, chr, strand, start, stop, atts):
+    self.gene_id = gene_id
+    self.gene_name = gene_name
+    self.chr = chr
+    self.strand = strand
+    self.start = int(start)-1
+    self.stop = int(stop)-1
+    self.transcripts = set()
+    self.atts = atts
+
+class Transcript:
+
+  def __init__(self, transcript_id, gene_id, start, stop):
+    self.transcript_id = transcript_id
+    self.gene = genes_db[gene_id]
+    self.start = int(start)-1
+    self.stop = int(stop)-1
+    self.utr3 = None
+    self.utr5 = None
+    self.exons = set()
+    self.gene.transcripts.add(self)
+
+class Exon:
+
+  def __init__(self, exon_id, transcript_id, start, stop):
+    self.exon_id = exon_id
+    self.transcript = transcripts_db[transcript_id]
+    self.start = int(start)-1
+    self.stop = int(stop)-1
+    self.transcript.exons.add(self)
+
+class Utr3:
+
+  def __init__(self, transcript_id, start, stop):
+    self.transcript = transcripts_db[transcript_id]
+    self.start = int(start)-1
+    self.stop = int(stop)-1
+    self.transcript.utr3 = self
+
+class Utr5:
+
+  def __init__(self, transcript_id, start, stop):
+    self.transcript = transcripts_db[transcript_id]
+    self.start = int(start)-1
+    self.stop = int(stop)-1
+    self.transcript.utr5 = self
 
 def init():
     load_chr_ucsc_ensembl()
@@ -46,6 +105,69 @@ def get_latest_version(species):
         return sd[species]
     else:
         return "ensembl90"
+
+def prepare(species="hg38", version=None):
+    if version==None:
+        version = get_latest_version(species)
+    print("[pybio] {species}: processing annotation".format(species=species))
+    annotation_folder = os.path.join(pybio.path.genomes_folder, "%s.annotation.%s" % (species, version))
+    f_log = open(os.path.join(annotation_folder, "log.txt"), "wt")
+    chrs_list = pybio.genomes.chr_list(species, version)
+
+    chr_list = set()
+    gtf_fields = ["chr", "source", "feature", "start", "stop", "a1", "strand", "a2", "atts"]
+    f = gzip.open(os.path.join(annotation_folder, "Homo_sapiens.GRCh38.109.chr.gtf.gz"), "rt")
+    clines = 0
+    r = f.readline()
+    while r:
+        r = r.replace("\r", "").replace("\n", "").split("\t")
+        if r[0].startswith("#"):
+            r = f.readline()
+            continue
+        data = dict(zip(gtf_fields, r))
+        atts = {}
+        temp = data["atts"].split("; ")
+        for el in temp:
+            el1, el2 = el.split(" ")[0], " ".join(el.split(" ")[1:])
+            atts[el1] = el2[1:-1]
+        if data["feature"] == "gene":
+            gene = Gene(atts["gene_id"], atts.get("gene_name", ""), data["chr"], data["strand"], data["start"], data["stop"], atts)
+            genes_db[atts["gene_id"]] = gene
+            chr_list.add(data["chr"])
+            gene_bins = set()
+            gene_start = int(data["start"])-1
+            gene_stop = int(data["stop"])-1
+            bin_min = math.floor(gene_start/gene_bin_size)
+            bin_max = math.floor(gene_stop/gene_bin_size)
+            for gene_bin_number in range(bin_min, bin_max+1):
+                gene_bins.add(gene_bin_number)
+            for gene_bin in gene_bins:
+                gene_bins_db.setdefault(data["chr"], {}).setdefault(gene_bin, set()).add(atts["gene_id"])
+        if data["feature"] == "transcript":
+            transcript = Transcript(atts["transcript_id"], atts["gene_id"], data["start"], data["stop"])
+            transcripts_db[atts["transcript_id"]] = transcript
+        if data["feature"] == "exon":
+            exon = Exon(atts["exon_id"], atts["transcript_id"], data["start"], data["stop"])
+            exons_db[atts["exon_id"]] = exon
+        if data["feature"] == "three_prime_utr":
+            utr3 = Utr3(atts["transcript_id"], data["start"], data["stop"])
+        if data["feature"] == "five_prime_utr":
+            utr5 = Utr5(atts["transcript_id"], data["start"], data["stop"])
+        r = f.readline()
+        clines += 1
+        if clines%100000==0:
+            print("%.2f M lines parsed, RAM GB used: %.3f" % (clines/1000000.0, process.memory_info().rss/1000000000))
+    f.close()
+    pickle.dump(gene_bins_db, open("gene_bins_db.pickle", "wb"))
+
+    print("#exons = ", len(exons_db.keys()))
+    print("#transcripts = ", len(transcripts_db.keys()))
+    print("#genes = ", len(genes_db.keys()))
+    print("chromosome list = ", "; ".join(list(chr_list)))
+
+    pickle.dump(exons_db, open("exons_db.pickle", "wb"))
+    pickle.dump(transcripts_db, open("transcripts_db.pickle", "wb"))
+    pickle.dump(genes_db, open("genes_db.pickle", "wb"))
 
 def genomes_list(version="ensembl90"):
     genomes = glob.glob(os.path.join(pybio.path.genomes_folder, "*.assembly.%s" % version))
@@ -145,7 +267,7 @@ def add_cluster(gid, dbgenes, dbclusters):
         cluster_start, cluster_stop = dbgenes[gid]["gene_start"], dbgenes[gid]["gene_stop"]
         dbclusters[(cluster_start, cluster_stop)] = cluster_genes # newly created cluster
 
-def prepare(species="hg19", version=None):
+def prepare_old(species="hg19", version=None):
 
     if version==None:
         version = get_latest_version(species)
