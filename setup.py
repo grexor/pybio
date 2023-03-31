ca73a0848aaeabcc90e56258bffafd47a93e6291
Gregor Rot <gregor.rot@gmail.com>
2023-03-31 13:45:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,20 @@ from setuptools import find_packages
 setup(
     name='pybio',
     version = "0.3",
-    #package_dir = {"":"src"},
-    packages=find_packages(),
+    package_dir = {"":"src"},
+    packages=find_packages("src"),
     description='pybio genomics',
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
     zip_safe=False,
     author='Gregor Rot',
-    scripts=["pybio"],
+    scripts=["src/pybio/pybio"],
     author_email='gregor.rot@gmail.com',
     url='https://github.com/grexor/pybio',
-    keywords=['pybio', 'bioinformatics']
+    keywords=['pybio', 'bioinformatics'],
+    include_package_data=True,
+    package_data={
+        'pybio': ['pybio.config.example'],
+    },
 )
