885f16459a18b7cade6736c51af80652f161b23e
Gregor Rot <gregor.rot@gmail.com>
2023-03-31 08:59:50 +0200

    Docs

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # https://packaging.python.org/en/latest/tutorials/packaging-projects/
 # https://stackoverflow.com/questions/50879668/python-setup-py-some-files-are-missing
 
-python -m build
+python setup.py bdist_wheel
--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import find_packages
 setup(
     name='pybio',
     version = "0.3",
-    package_dir = {"":"src"},
-    packages=find_packages("src"),
+    #package_dir = {"":"src"},
+    packages=find_packages(),
     description='pybio genomics',
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
     zip_safe=False,
     author='Gregor Rot',
-    scripts=["src/pybio/pybio"],
+    scripts=["pybio"],
     author_email='gregor.rot@gmail.com',
     url='https://github.com/grexor/pybio',
     keywords=['pybio', 'bioinformatics']
